d9b93c1858c1238f1d6609df218f2069c668c24a
vadim-ggt <koval.cap.stop.j@gmail.com>
2026-02-21 20:44:15 +0300

    impl db schema

---
+++ src/main/java/com/innowise/orderservice/domain/entity/OrderItem.java
@@ -0,0 +1,30 @@
+package com.innowise.orderservice.domain.entity;
+
+import com.innowise.orderservice.domain.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
