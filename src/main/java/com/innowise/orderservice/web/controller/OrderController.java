b731e42a8b1fc460354f5281f8ec78658bbe13a8
vadim-ggt <koval.cap.stop.j@gmail.com>
2026-03-25 15:10:28 +0300

    fix requestmapping

--- src/main/java/com/innowise/orderservice/web/controller/OrderController.java
+++ src/main/java/com/innowise/orderservice/web/controller/OrderController.java
@@ -23,7 +23,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/orders")
+@RequestMapping("/api/orders")
 @RequiredArgsConstructor
 public class OrderController {
 
